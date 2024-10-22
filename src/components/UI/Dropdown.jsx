295912a9871b15045bb7cacb82816e3e3ec45b81
Edwilson <edwsilva@gmail.com>
2024-10-22 15:41:05 -0300

    Ajute menu IA

--- src/components/UI/Dropdown.jsx
+++ src/components/UI/Dropdown.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import { FaMagic } from "react-icons/fa";
 import { Dropdown } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "./dropDownButton.css"; // Importar o arquivo CSS
 
 const DropDownButton = () => {
   const navigate = useNavigate();
 
   return (
     <Dropdown>
-      <Dropdown.Toggle variant="primary" id="dropdown-basic">
-        Ações
+      <Dropdown.Toggle variant="primary" id="dropdown-basic" title="IA">
+        <FaMagic /> {/* Ícone FaMagic */}
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={() => navigate("/")}>
-          Detalhamento da Empresa
-        </Dropdown.Item>
-        <Dropdown.Item onClick={() => navigate("/action-2")}>
+      <Dropdown.Menu style={{ backgroundColor: "#0056b3" }}>
+        <Dropdown.Item
+          onClick={() => navigate("/action-2")}
+          style={{ color: "#ffffff" }}
+          className="dropdown-item-hover"
+        >
           Perfil com IA
         </Dropdown.Item>
-        <Dropdown.Item onClick={() => navigate("/action-3")}>
+        <Dropdown.Item
+          onClick={() => navigate("/action-3")}
+          style={{ color: "#ffffff" }}
+          className="dropdown-item-hover"
+        >
           Resumo de Depoimentos com IA
         </Dropdown.Item>
       </Dropdown.Menu>
